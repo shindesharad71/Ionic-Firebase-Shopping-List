6e8db2fedca809228c60a4f65b6d4fe1412551b0
Sharad Shinde <shindesharad71@gmail.com>
2017-09-29 19:54:05 +0530

    display item list

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
 import { AddShoppingPage } from '../add-shopping/add-shopping';
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
 
 @Component({
   selector: 'page-shopping-list',
@@ -8,7 +10,10 @@ import { AddShoppingPage } from '../add-shopping/add-shopping';
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  shoppingListRef$: FirebaseListObservable<ShoppingItem[]>
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private database: AngularFireDatabase) {
+    this.shoppingListRef$ = this.database.list('shopping-list');
   }
 
   toShopping() {
