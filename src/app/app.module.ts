31712cbefa822264f49542f9fbde69426d0d0309
Sharad Shinde <shindesharad71@gmail.com>
2017-09-29 16:56:54 +0530

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FIREBASE } from './firebase';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.ts
@@ -0,0 +1,8 @@
+export const FIREBASE = {
+  apiKey: "AIzaSyBF1ccIWLRKGcf_Qc1bzp4kgi4KZYSSnlc",
+  authDomain: "fir-crud-6f11f.firebaseapp.com",
+  databaseURL: "https://fir-crud-6f11f.firebaseio.com",
+  projectId: "fir-crud-6f11f",
+  storageBucket: "fir-crud-6f11f.appspot.com",
+  messagingSenderId: "335163227620"
+};
