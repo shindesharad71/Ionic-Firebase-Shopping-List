524d0e7a7ed356afa642de6daf8065a1669380d3
Sharad Shinde <shindesharad71@gmail.com>
2017-09-29 19:17:51 +0530

    input filed values action set

--- src/pages/add-shopping/add-shopping.ts
+++ src/pages/add-shopping/add-shopping.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
 
 @Component({
   selector: 'page-add-shopping',
@@ -7,11 +8,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddShoppingPage {
 
+  shoppingItem = {} as ShoppingItem;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddShoppingPage');
+  addItem(shoppingItem: ShoppingItem) {
+    console.log(shoppingItem);
   }
 
 }
